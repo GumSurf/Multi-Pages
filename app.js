d3eab8c1f2edd13811d13d2ffb968c558cdab317
GumSurf <gabriel.christe@gmail.com>
2023-11-29 16:42:36 +0100

    Add: restart on new file

---
+++ app.js
@@ -0,0 +1,22 @@
+function init() {
+    createScene();
+    setupWindowManager();
+    resize();
+    renderScene();
+}
+
+function createScene() {
+
+}
+
+function setupWindowManager() {
+
+}
+
+function renderScene() {
+
+}
+
+function resize() {
+
+}
