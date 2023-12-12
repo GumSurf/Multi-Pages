99ccd2311f3f1d29842900d2f3fb2fcce502d17c
GumSurf <gabriel.christe@gmail.com>
2023-12-12 15:49:33 +0100

    delete: console.log

--- windowManager.js
+++ windowManager.js
@@ -31,7 +31,6 @@ window.addEventListener('beforeunload', function (e) {
 
 export function init() {
     const storedWindows = getStoredWindows() || [];
-    console.log("storedWindows =", storedWindows);
     let count = localStorage.getItem("count") || 0;
     count++;
     idWindow = count;
